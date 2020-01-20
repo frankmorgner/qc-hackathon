5e99ac134a5ba085a90880cfd8e4a39d331166a3
Christian <christian@avanetix.io>
2020-01-20 19:36:37 +0100

    changed run.sh deleting the host parameter

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t avanetix_qws -f config/avanetix_qws.Dockerfile .
-docker run -it --net=host -v ${PWD}:/usr/local/bin/avanetix_qws -p 8888:8888 avanetix_qws
+docker run -it -v ${PWD}:/usr/local/bin/avanetix_qws -p 8888:8888 avanetix_qws
